c10c1b92e6c41cd8b1ea1a5c9dba03e1d5dc161a
khadija-ahmadova <xadija.ahmedova@gmail.com>
2025-07-02 21:03:49 +0400

    create models for input and output

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 import os
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # initialize app
 app = FastAPI()
+
+# --- pydantic models ---
+class ChatRequest(BaseModel):
+    prompt: str # {"prompt" : "..."}
+
+class ChatResponse(BaseModel):
+    response: str # return {"response": "..."}
